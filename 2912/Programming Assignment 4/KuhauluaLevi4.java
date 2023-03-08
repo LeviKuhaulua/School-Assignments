a6c9481fc4d80f37742184076802e0f8154d91dc
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-03-08 10:49:34 -1000

    First Commit:
    Setting up a template for the next assignment in CSCI 2912

--- 2912/Programming Assignment 4/KuhauluaLevi4.java
+++ 2912/Programming Assignment 4/KuhauluaLevi4.java
@@ -5,7 +5,9 @@
  * @date
  * @assignment Programming Assignment 4
  */
-
+// Setting up for the next programming assignment; 
+import java.util.*; 
+import java.io.*; 
 public class KuhauluaLevi4 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
