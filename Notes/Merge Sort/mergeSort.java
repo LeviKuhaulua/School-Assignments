3c5f231a4da1de03b1e1c953b28e4992d3244832
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-03-22 21:21:21 -1000

    Second Commit:
    Finished Merge Sort Algorithm

--- Notes/Merge Sort/mergeSort.java
+++ Notes/Merge Sort/mergeSort.java
@@ -1,6 +1,6 @@
 /*
  * Merge Sort Algorithm: 
- * Needs >> Unsorted Array 
+ * Needs >> Unsorted Array, Helper Array
  * 
  * Principles: 
  * Take the unsorted array 
@@ -11,13 +11,24 @@ public class mergeSort {
     public static void main(String[] args) {
         int[] array = {14, 91, -52, 79, 47, 86, 75, 36, -26, -44};
         MergeSort(array); 
+        for (int i : array) System.out.println(i);
     }
 
+    /**
+     * MergeSort - allocates a helper array to help with sorting then calls a helper method
+     * @param array - unsorted array
+     */
     public static void MergeSort(int[] array) {
-        int[] helperArray = new int[array.length - 1]; 
+        int[] helperArray = new int[array.length]; 
         MergeSort(array, helperArray, 0, (array.length - 1)); 
     }
-
+    /**
+     * MergeSort - helper method that will sort the array. 
+     * @param array - unsorted array
+     * @param helper - helper array for sorting 
+     * @param min - minimum index of the array 
+     * @param max - maximum index of the array 
+     */
     private static void MergeSort(int[] array, int[] helper, int min, int max) {
         // Base Case
         if (min == max) return; 
@@ -33,8 +44,36 @@ public class mergeSort {
 
         // After Action
         // Copy elements into helper array
-        for (int i : array) helper[i] = i; 
+        for (int i = 0; i < array.length; i++) helper[i] = array[i]; 
 
         // Compare Elements
+        int leftIndex = min; 
+        int rightIndex = middleRight; 
+        int dst = min; // for sorted array
+
+        while (leftIndex <= middleLeft && rightIndex <= max) {
+            if(helper[leftIndex] < helper[rightIndex]) {
+                array[dst] = helper[leftIndex]; 
+                leftIndex++; 
+            } else {
+                array[dst] = helper[rightIndex]; 
+                rightIndex++; 
+            }
+            dst++; 
+        }
+
+        // Add leftovers of left partition
+        while (leftIndex <= middleLeft) {
+            array[dst] = helper[leftIndex];
+            leftIndex++;
+            dst++; 
+        }
+
+        // Add leftovers of right partition
+        while (rightIndex <= max) {
+            array[dst] = helper[rightIndex]; 
+            rightIndex++;
+            dst++;  
+        }
     }
 }
