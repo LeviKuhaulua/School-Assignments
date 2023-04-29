4cf6ff2d5af51ea26f05ff982812d1daadec4f10
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-04-28 21:30:01 -1000

    Added mnemonic and accelerator keys for menu items

--- 2912/Programming Assignment 5/textEditor.java
+++ 2912/Programming Assignment 5/textEditor.java
@@ -33,6 +33,7 @@ public class textEditor extends JFrame {
     private void configureScrollPane() {
         textFileContents = new JTextArea(); // Initializing text area 
         textFileContents.setLineWrap(false); // not allowing line wrap when user adds or edits texts - helps allow scroll bars 
+        textFileContents.setFont(new Font("Monospace", Font.PLAIN, 14)); 
         textFileScrollPane = new JScrollPane(textFileContents); // add text area to the scroll pane 
         this.setContentPane(textFileScrollPane); // setting the whole frame to be the text editor 
     }
@@ -41,16 +42,36 @@ public class textEditor extends JFrame {
      * setUpMenu is a private method that will configure the menu bar, menu, and menu items for the program 
      */
     private void setUpMenu() {
+        final int shortcutKeyMask = Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx(); 
         // Creating Menu 
         fileToolMenu = new JMenuBar(); 
 
         // Creating Menu and Menu Items 
         fileMenu = new JMenu("File"); 
+         
         createTxtFile = new JMenuItem("New"); 
+         
         openTxtFile = new JMenuItem("Open"); 
+         
         saveTxtFile = new JMenuItem("Save"); 
+         
         saveTxtFileAs = new JMenuItem("Save As..."); 
+         
         exitMenu = new JMenuItem("Exit"); 
+        
+        // Setting Mnemonic keys for each file menu 
+        fileMenu.setMnemonic(KeyEvent.VK_F); // Alt+f for file
+        createTxtFile.setMnemonic(KeyEvent.VK_N); // n for new 
+        openTxtFile.setMnemonic(KeyEvent.VK_O); // o for open 
+        saveTxtFile.setMnemonic(KeyEvent.VK_S); // s for save
+        saveTxtFileAs.setMnemonic(KeyEvent.VK_A); // a for save as
+        exitMenu.setMnemonic(KeyEvent.VK_X); // x for exit 
+
+        // Setting Accelerator for each menu item 
+        createTxtFile.setAccelerator(KeyStroke.getKeyStroke('N', shortcutKeyMask));
+        openTxtFile.setAccelerator(KeyStroke.getKeyStroke('O', shortcutKeyMask));
+        saveTxtFile.setAccelerator(KeyStroke.getKeyStroke('S', shortcutKeyMask)); 
+        saveTxtFileAs.setAccelerator(KeyStroke.getKeyStroke('S', shortcutKeyMask + KeyEvent.SHIFT_DOWN_MASK)); 
 
         // Adding menu items to the menu bar
         fileMenu.add(createTxtFile); 
