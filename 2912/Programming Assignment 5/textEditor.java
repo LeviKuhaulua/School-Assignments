c505560b072430c088b8dac6a8ecc3f749278c59
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-04-28 20:47:56 -1000

    Created the text editor frame and added the menu component (no code yet)

--- 2912/Programming Assignment 5/textEditor.java
+++ 2912/Programming Assignment 5/textEditor.java
@@ -3,27 +3,66 @@ import java.awt.*;
 import java.awt.event.*; 
 
 public class textEditor extends JFrame {
-    private JFrame textEditorFrame; // Frame for the text-editor 
     private JScrollPane textFileScrollPane; // Scroll pane to show contents of text files
-    private JTextArea textFileContents; // to put text file content or have user create text file 
+    private JTextArea textFileContents; // to put text file content or have user create text file  
     // Menu components 
     private JMenuBar fileToolMenu; 
-    private JMenu FileMenu; 
+    private JMenu fileMenu; 
     private JMenuItem createTxtFile, openTxtFile, saveTxtFile, saveTxtFileAs, exitMenu; 
 
-    private String title = " - CSCI 2912 Editor"; // Create title to include the 
+    private final String title = " - CSCI 2912 Editor"; // Title will end with " - CSCI 2912 Editor" 
 
     public textEditor() {
-        textEditorFrame = new JFrame(); 
-        textEditorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        textEditorFrame.setSize(new Dimension(600, 600));
-        textEditorFrame.setLocationRelativeTo(null); 
-        textEditorFrame.setLayout(new GridLayout()); 
-        textEditorFrame.setTitle("Untitled".concat(title)); 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        this.setSize(new Dimension(500, 500)); 
+        this.setLocationRelativeTo(null);
+        this.setLayout(new FlowLayout()); 
+        this.setTitle("Untitled".concat(title));
+        this.setResizable(true);
 
-        textFileScrollPane = new JScrollPane(); 
         
-        textEditorFrame.add(textFileScrollPane); 
-        textEditorFrame.setVisible(true); 
+        configureScrollPane(); 
+        setUpMenu(); 
+         
+        this.setVisible(true); 
     }
+
+    /**
+     * configureScrollPane is a private method to configure the text editor contents which is the scroll pane and 
+     */
+    private void configureScrollPane() {
+        textFileContents = new JTextArea(); // Initializing text area 
+        textFileContents.setLineWrap(false); // not allowing line wrap when user adds or edits texts - helps allow scroll bars 
+        textFileScrollPane = new JScrollPane(textFileContents); // add text area to the scroll pane 
+        this.setContentPane(textFileScrollPane); // setting the whole frame to be the text editor 
+    }
+
+    /**
+     * setUpMenu is a private method that will configure the menu bar, menu, and menu items for the program 
+     */
+    private void setUpMenu() {
+        // Creating Menu 
+        fileToolMenu = new JMenuBar(); 
+
+        // Creating Menu and Menu Items 
+        fileMenu = new JMenu("File"); 
+        createTxtFile = new JMenuItem("New"); 
+        openTxtFile = new JMenuItem("Open"); 
+        saveTxtFile = new JMenuItem("Save"); 
+        saveTxtFileAs = new JMenuItem("Save As..."); 
+        exitMenu = new JMenuItem("Exit"); 
+
+        // Adding menu items to the menu bar
+        fileMenu.add(createTxtFile); 
+        fileMenu.add(openTxtFile); 
+        fileMenu.addSeparator(); 
+        fileMenu.add(saveTxtFile); 
+        fileMenu.add(saveTxtFileAs); 
+        fileMenu.addSeparator(); 
+        fileMenu.add(exitMenu); 
+        fileToolMenu.add(fileMenu); 
+       
+        // Setting menu bar to the frame 
+        this.setJMenuBar(fileToolMenu);
+    }    
 }
