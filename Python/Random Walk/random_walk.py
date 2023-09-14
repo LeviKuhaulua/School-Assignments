da443f4a285e450883a22a9c41249e496d94e80e
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-09-13 16:52:05 -1000

    Started Random Walk assignment

---
+++ Python/Random Walk/random_walk.py
@@ -0,0 +1,23 @@
+'''
+Author: Levi Kuhaulua
+Course: Python 3371
+Professor: Dr. Crawford 
+09-13-2023
+
+Random Walk Assignment: 
+
+Turtle will choose a random direction to walk in and repeat that process until they reach back to their original position. 
+'''
+
+from turtle import Turtle, Screen
+from random import choice
+
+# Initialize Turtle and Screen 
+awiwi = Turtle() 
+awiwi_hale = Screen() 
+
+
+
+
+# So that the turtle graphics do not disappear at the end. 
+awiwi_hale.mainloop() 
