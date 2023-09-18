c52bf875459ceebc00d08a21014768ac222270f4
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-09-18 11:03:22 -1000

    Moved Java notes to Coding-Notes repository

--- Notes/Inheritance/Inheritance Practice/Publisher.java
+++
@@ -1,8 +0,0 @@
-/**
- * Publisher Interface 
- * Allow to change and get a publisher of a publication
- */
-public interface Publisher {
-    public void setPublisher(String publisher); 
-    public String getPublisher(); 
-}
