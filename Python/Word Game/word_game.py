7b3b757a57d01bff6b7fd42143688de4459f4e86
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-09-23 20:51:04 -1000

    Finished first solution, code will:
    1. Get the word from WordEn.txt
    2. Get the characters of that word and check if Accurate has enough characters to make word
    3. Validate if the word is real before adding to the container
    4. Output words that can be made from Accurate

--- Python/Word Game/word_game.py
+++ Python/Word Game/word_game.py
@@ -1,20 +1,22 @@
 days_word = "accurate"
 
+derived_words = set() # set to hold all the words that can be derived from 'Accurate'
 
-combo_words = set() # set to hold all the words that can be derived from 'Accurate'
+eng_dict = (word.strip() for word in open("WordEn.txt", "r")) # Generator Expression for WordEn.txt (HUUUUGE FILE)
 
-eng_dict = (word.strip() for word in open("testWords.txt", "r")) # Generator Expression that will eventually be used for WordEn.txt (HUUUUUGE FILE)
-
-# Solution 1: Takes the list of words from a text file (in this case: testWords.txt) and sees if it can be derived from 'Accurate'. 
+# Solution 1: Grabs the characters from the words in WordEn.txt file and checks if it can be made from 'Accurate'
 
 for word in eng_dict: 
-    build_word = ""
-    for letter in word:  
+    build_word = "" # Reset and try to build a new word
+    for letter in word: 
         letter_freq = word.count(letter)
         if letter in word and days_word.count(letter) >= letter_freq: build_word += letter
-    if len(build_word) >= 4: combo_words.add(build_word)
-    
+    # Validates if the word is a valid word before adding to the set. 
+    if build_word == word and len(build_word) >= 4: derived_words.add(build_word) 
 
-print(combo_words) # Output should be: curate, rate, erat, tear. Excluded: rater, rates
+
+print('Words derived from Accurate')
+for word in derived_words: print(word)
+
 
             
