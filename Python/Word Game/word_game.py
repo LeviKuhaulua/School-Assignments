b94086da875f52d1d8cf4a275593c31471b42354
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-09-23 20:09:35 -1000

    Changed comments to reflect what I'm testing (see last commit)

--- Python/Word Game/word_game.py
+++ Python/Word Game/word_game.py
@@ -5,14 +5,13 @@ combo_words = set() # set to hold all the words that can be derived from 'Accura
 
 eng_dict = (word.strip() for word in open("testWords.txt", "r")) # Generator Expression that will eventually be used for WordEn.txt (HUUUUUGE FILE)
 
-# Solution 1: Takes the list (for now 'sample_test') - then checks the character for each word and sees if Accurate can supply those letters
+# Solution 1: Takes the list of words from a text file (in this case: testWords.txt) and sees if it can be derived from 'Accurate'. 
 
 for word in eng_dict: 
     build_word = ""
     for letter in word:  
         letter_freq = word.count(letter)
-        if letter in word and days_word.count(letter) >= letter_freq: 
-            build_word += letter   
+        if letter in word and days_word.count(letter) >= letter_freq: build_word += letter
     if len(build_word) >= 4: combo_words.add(build_word)
     
 
