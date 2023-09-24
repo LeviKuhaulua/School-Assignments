54c22c78e57c633106ae037e59747d37d1606b6a
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-09-23 22:24:51 -1000

    Cleaned up code for Solution One and starting on code for Solution Two

--- Python/Word Game/word_game.py
+++ Python/Word Game/word_game.py
@@ -4,19 +4,50 @@ derived_words = set() # set to hold all the words that can be derived from 'Accu
 
 eng_dict = (word.strip() for word in open("WordEn.txt", "r")) # Generator Expression for WordEn.txt (HUUUUGE FILE)
 
+sample_list = ["aurae", "aurate", "advark", "cart", "crate","curl", "urea", "umbral", "rate", "race", "raters", "tare", "tear", "treat", "eatery", "enter", "eruct"]
+
+# Solution Two: Takes characters from 'accurate' and sees what words it can make
+
+'''for letter in days_word: 
+    possible_words = [list(word) for word in sample_list if word.startswith(letter)] # generate a list of possible words based on character
+    for word in possible_words: 
+        if letter '''
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Solution 1: Grabs the characters from the words in WordEn.txt file and checks if it can be made from 'Accurate'
 
 for word in eng_dict: 
     build_word = "" # Reset and try to build a new word
     for letter in word: 
         letter_freq = word.count(letter)
-        if letter in word and days_word.count(letter) >= letter_freq: build_word += letter
+        # Checks if 'Accurate' has that letter. Also accounts if there is multiple same letters. 
+        if days_word.count(letter) >= letter_freq: build_word += letter 
     # Validates if the word is a valid word before adding to the set. 
-    if build_word == word and len(build_word) >= 4: derived_words.add(build_word) 
+    if build_word == word and len(build_word) >= 4 and build_word != 'accurate': derived_words.add(build_word) 
 
 
 print('Words derived from Accurate')
 for word in derived_words: print(word)
-
+print(len(derived_words))
 
             
