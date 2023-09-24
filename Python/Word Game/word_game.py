91bd401bf81137811916128a245185fecdaefcfc
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-09-23 23:22:41 -1000

    Finished both solutions. Working on cleaning up code

--- Python/Word Game/word_game.py
+++ Python/Word Game/word_game.py
@@ -2,32 +2,10 @@ days_word = "accurate"
 
 derived_words = set() # set to hold all the words that can be derived from 'Accurate'
 
-eng_dict = (word.strip() for word in open("WordEn.txt", "r")) # Generator Expression for WordEn.txt (HUUUUGE FILE)
-
-
-
-# Solution 2: Takes characters from 'accurate' and sees what words it can make
-
-for letter in days_word: 
-    possible_words = [word.strip() for word in open("testWords.txt", "r") if word.startswith(letter) and len(word) >= 4]
-    
-    for word in possible_words: 
-        build_word = ""
-        for letter in word: 
-            letter_freq = word.count(letter)
-            if days_word.count(letter) >= letter_freq: build_word += letter
-        if build_word == word and build_word != days_word: derived_words.add(build_word)
-
-print(derived_words) # Desired Output: aurae, aurate, cart, crate, urea, rate, race, tare, tear, eruct
-print(len(derived_words))
-        
-    
-
-
 
 # Solution 1: Grabs the characters from the words in WordEn.txt file and checks if it can be made from 'Accurate'
 
-'''for word in eng_dict: 
+for word in (word.strip() for word in open("WordEn.txt", "r")): 
     build_word = "" # Reset and try to build a new word
     for letter in word: 
         letter_freq = word.count(letter)
@@ -39,6 +17,33 @@ print(len(derived_words))
 
 print('Words derived from Accurate')
 for word in derived_words: print(word)
-print(len(derived_words))'''
+print(len(derived_words))
+
+
+# Solution 2: Takes characters from 'accurate' and sees what words it can make
+
+for letter in days_word: 
+    # Generates possible list of words based on index of days_word
+    possible_words = [word for word in (word.strip() for word in open("WordEn.txt", "r")) if word.startswith(letter) and len(word) >= 4] 
+    
+
+    for word in possible_words: 
+        build_word = ""
+        for letter in word: 
+            letter_freq = word.count(letter)
+            # Checks if accurate has that letters. Also accounts if there is multiple same letters. 
+            if days_word.count(letter) >= letter_freq: build_word += letter
+        # Validates if word is real and not 'accurate' before adding to the set. 
+        if build_word == word and build_word != days_word: derived_words.add(build_word)
+
+print('Words derived from Accurate')
+for word in derived_words: print(word)
+print(len(derived_words))
+        
+    
+
+
+
+
 
             
