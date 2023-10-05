4316c33d4bed88fc055852b7b9fac8a348facb4e
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2023-10-05 10:40:12 -1000

    Made code for Luhn Algorithm more readable

--- Python/Luhn Algorithm/luhn_algorithm.py
+++ Python/Luhn Algorithm/luhn_algorithm.py
@@ -4,11 +4,13 @@
 # Luhn's Algorithm 
 # Create a program that will validate the Check-Digit number of an account number. Assume that check-digit number is the last digit.
 
-id = '37648' 
+id = '4388576053793488' 
 # Intialized Dictionary containing values when doing Luhn's Algorithm. 
 luhn_v = {"0": 0, "1": 2, "2": 4, "3": 6, "4": 8, \
     "5": 1, "6": 3, "7": 5, "8": 7, "9": 9}
-if 10 - (sum([ luhn_v.get(x) for x in id[-2::-2]] + [ int(x) for x in id[-3::-2]]) % 10) == int(id[-1]): print('it matches up') 
+if 10 - (sum([ luhn_v.get(x) for x in id[-2::-2]] \
+     + [ int(x) for x in id[-3::-2]]) % 10) \
+     == int(id[-1]): print('it matches up') 
 else: print("it doesn't match up") 
 
 
