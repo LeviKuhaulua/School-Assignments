d3bf390eb7c92541eb08f069c9a35005f1a9ef38
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-02-01 07:50:59 -1000

    Revert "Testing cases for constructors"
    This reverts commit 6f6be4e54a388afdb3bc76fd46bae72f7d15b3b8.

--- Data Structures/Homework 2 - Statistician/StatisticianDemo.java
+++ Data Structures/Homework 2 - Statistician/StatisticianDemo.java
@@ -1,9 +1,7 @@
-import java.util.Scanner; 
+
+
 public class StatisticianDemo {
     public static void main(String[] args) {
-        // Statistician zeroCapacity = new Statistician(0); // Expected Output: IllegalArgumentException 
-        // Statistician negativeCapacity = new Statistician(-1); // Expected Output: NegativeArraySizeException
-        // Statistician notEnoughCapacity = new Statistician(Integer.MAX_VALUE); // Expected Output: OutOfMemoryError
-        Statistician okayCapacity = new Statistician(15); // Expected Output: Okay
+        
     }
 }
