d5f16a3e5af78eb8365273f045c142a591bb600c
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-02-01 19:07:58 -1000

    Tested length

--- Data Structures/Homework 2 - Statistician/StatisticianDemo.java
+++ Data Structures/Homework 2 - Statistician/StatisticianDemo.java
@@ -3,5 +3,16 @@
 public class StatisticianDemo {
     public static void main(String[] args) {
         
+        /* LENGTH TESTING*/
+        
+        Statistician lenStatistician = new Statistician(); 
+
+        System.out.println(lenStatistician.length()); // Expected Output: 0 
+
+        lenStatistician.nextNumber(1.1); 
+
+        System.out.println(lenStatistician.length()); // Expected Output: Length should increment
+
+        /* END OF LENGTH TESTING */
     }
 }
