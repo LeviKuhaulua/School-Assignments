98902560fb6f5e98b93ae51a779cd6cb96c821d6
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-02-01 22:15:31 -1000

    Cleaned up test file

--- Data Structures/Homework 2 - Statistician/StatisticianDemo.java
+++ Data Structures/Homework 2 - Statistician/StatisticianDemo.java
@@ -14,12 +14,6 @@ public class StatisticianDemo {
         System.out.println(lenStatistician.length()); // Expected Output: Length should increment
 
         /* END OF LENGTH TESTING */
-        System.out.println(lenStatistician.length()); // Expected Output: 1 
-        lenStatistician.nextNumber(-8.9); 
-        lenStatistician.nextNumber(10.22); 
-        System.out.println(lenStatistician.length()); // Expected Output: 3
-
-        /* END OF LENGTH TESTING */
 
         /* LAST NUMBER TESTING */
        
@@ -57,16 +51,6 @@ public class StatisticianDemo {
         sumStatistician.nextNumber(-9.0); 
         System.out.println(sumStatistician.sum()); // Expected Output: 8.0 
         /* END OF SUM TESTING */
-        /* LENGTH TESTING*/
         
-        Statistician lenStatistician = new Statistician(); 
-
-        System.out.println(lenStatistician.length()); // Expected Output: 0 
-
-        lenStatistician.nextNumber(1.1); 
-
-        System.out.println(lenStatistician.length()); // Expected Output: Length should increment
-
-        /* END OF LENGTH TESTING */
     }
 }
