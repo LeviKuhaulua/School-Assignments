e7259c7f0055c10e6d6e95b25c5c2487005622d1
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-02-05 19:04:42 -1000

    Tested mean method for Statistician

--- Data Structures/Homework 2 - Statistician/StatisticianDemo.java
+++ Data Structures/Homework 2 - Statistician/StatisticianDemo.java
@@ -9,89 +9,112 @@ public class StatisticianDemo {
         
         /* LENGTH TESTING*/
         
-        Statistician lenStatistician = new Statistician(); 
+        // Statistician lenStatistician = new Statistician(); 
 
-        System.out.println(lenStatistician.length()); // Expected Output: 0 
+        // System.out.println(lenStatistician.length()); // Expected Output: 0 
 
-        lenStatistician.nextNumber(1.1); 
+        // lenStatistician.nextNumber(1.1); 
 
-        System.out.println(lenStatistician.length()); // Expected Output: Length should increment
+        // System.out.println(lenStatistician.length()); // Expected Output: Length should increment
 
         /* END OF LENGTH TESTING */
 
         /* LAST NUMBER TESTING */
        
-        Statistician lastNumStatistician = new Statistician(); 
+        // Statistician lastNumStatistician = new Statistician(); 
 
-        System.out.println(lastNumStatistician.lastNumber()); // Expected Output: Double.NaN
+        // System.out.println(lastNumStatistician.lastNumber()); // Expected Output: Double.NaN
         
-        lastNumStatistician.nextNumber(10.22); 
+        // lastNumStatistician.nextNumber(10.22); 
 
-        System.out.println(lastNumStatistician.lastNumber()); // Expected Output: 10.22
+        // System.out.println(lastNumStatistician.lastNumber()); // Expected Output: 10.22
 
-        lastNumStatistician.nextNumber(-0.2222); 
-        System.out.println(lastNumStatistician.lastNumber()); // Expected Output: -0.2222. 
+        // lastNumStatistician.nextNumber(-0.2222); 
+        // System.out.println(lastNumStatistician.lastNumber()); // Expected Output: -0.2222. 
 
-        lastNumStatistician.nextNumber(1.02); 
-        lastNumStatistician.nextNumber(5.87); 
-        lastNumStatistician.nextNumber(-1.22); 
-        System.out.println(lastNumStatistician.lastNumber()); // Expected Output: -1.22
+        // lastNumStatistician.nextNumber(1.02); 
+        // lastNumStatistician.nextNumber(5.87); 
+        // lastNumStatistician.nextNumber(-1.22); 
+        // System.out.println(lastNumStatistician.lastNumber()); // Expected Output: -1.22
 
 
         /* END OF LAST NUMBER TESTING */
 
         /* SUM TESTING */
-        Statistician sumStatistician = new Statistician(); 
+        // Statistician sumStatistician = new Statistician(); 
 
-        System.out.println(sumStatistician.sum()); // Expected Output: 0; 
+        // System.out.println(sumStatistician.sum()); // Expected Output: 0; 
 
-        sumStatistician.nextNumber(10.0); 
-        System.out.println(sumStatistician.sum()); // Expected Output: 10
+        // sumStatistician.nextNumber(10.0); 
+        // System.out.println(sumStatistician.sum()); // Expected Output: 10
         
-        sumStatistician.nextNumber(5.0); 
-        sumStatistician.nextNumber(2.0);
-        System.out.println(sumStatistician.sum()); // Expected Output: 17.0
+        // sumStatistician.nextNumber(5.0); 
+        // sumStatistician.nextNumber(2.0);
+        // System.out.println(sumStatistician.sum()); // Expected Output: 17.0
 
-        sumStatistician.nextNumber(-9.0); 
-        System.out.println(sumStatistician.sum()); // Expected Output: 8.0 
+        // sumStatistician.nextNumber(-9.0); 
+        // System.out.println(sumStatistician.sum()); // Expected Output: 8.0 
         /* END OF SUM TESTING */
 
         /* MAXIMUM NUMBER TESTING */
 
-        Statistician maxStatistician = new Statistician(); 
+        // Statistician maxStatistician = new Statistician(); 
 
-        System.out.println(maxStatistician.maximum()); // Expected Output: Double.NaN
+        // System.out.println(maxStatistician.maximum()); // Expected Output: Double.NaN
 
-        maxStatistician.nextNumber(10.0); 
+        // maxStatistician.nextNumber(10.0); 
 
-        System.out.println(maxStatistician.maximum()); // Expected output: 10.0; 
+        // System.out.println(maxStatistician.maximum()); // Expected output: 10.0; 
 
-        maxStatistician.nextNumber(5.0); 
-        System.out.println(maxStatistician.maximum()); // Expected Output: 10.0; 
-        maxStatistician.nextNumber(100.0); 
-        maxStatistician.nextNumber(-100.0); 
-        maxStatistician.nextNumber(-90.99); 
-        System.out.println(maxStatistician.maximum()); // Expected Output: 100.0;
+        // maxStatistician.nextNumber(5.0); 
+        // System.out.println(maxStatistician.maximum()); // Expected Output: 10.0; 
+        // maxStatistician.nextNumber(100.0); 
+        // maxStatistician.nextNumber(-100.0); 
+        // maxStatistician.nextNumber(-90.99); 
+        // System.out.println(maxStatistician.maximum()); // Expected Output: 100.0;
         
 
         /* END OF MAX NUMBER TESTING */
         
         /* MIN NUMBER TESTING */
-        Statistician minStatistician = new Statistician(); 
+        // Statistician minStatistician = new Statistician(); 
 
-        System.out.println(minStatistician.minimum()); // Expected Output: Double.NaN
+        // System.out.println(minStatistician.minimum()); // Expected Output: Double.NaN
 
-        minStatistician.nextNumber(1.0); 
-        System.out.println(minStatistician.minimum()); // Expected Output: 1.0
+        // minStatistician.nextNumber(1.0); 
+        // System.out.println(minStatistician.minimum()); // Expected Output: 1.0
 
-        minStatistician.nextNumber(10.0);
-        System.out.println(minStatistician.minimum()); // Expected Output: 1.0
+        // minStatistician.nextNumber(10.0);
+        // System.out.println(minStatistician.minimum()); // Expected Output: 1.0
 
-        minStatistician.nextNumber(10.0);
-        minStatistician.nextNumber(-10.0);
-        minStatistician.nextNumber(-100.1);
+        // minStatistician.nextNumber(10.0);
+        // minStatistician.nextNumber(-10.0);
+        // minStatistician.nextNumber(-100.1);
 
-        System.out.println(minStatistician.minimum()); // Expected Output: -100.1
+        // System.out.println(minStatistician.minimum()); // Expected Output: -100.1
         /* END OF MIN NUMBER TESTING */
+
+        /* MEAN TESTING */
+
+        Statistician meanStatistician = new Statistician(); 
+
+        System.out.println(meanStatistician.mean());
+
+        meanStatistician.nextNumber(10.0); 
+
+        System.out.println(meanStatistician.mean()); // Expected Output: 10.0
+
+        meanStatistician.nextNumber(5.0); 
+        meanStatistician.nextNumber(5.0); 
+        meanStatistician.nextNumber(4.0); 
+        System.out.println(meanStatistician.mean()); // Expected Output: 6.0
+
+        meanStatistician.nextNumber(-1.0); 
+        meanStatistician.nextNumber(-46.0);
+        meanStatistician.nextNumber(-12.0);
+        System.out.println(meanStatistician.mean()); // Expected Output: -5.0
+
+
+        /* END OF MEAN TESTING */
     }
 }
