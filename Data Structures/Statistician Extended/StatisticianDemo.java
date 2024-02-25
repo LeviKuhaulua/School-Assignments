a9958e0c7d0e0c683ec3047bc0511a905c914b41
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-02-25 13:58:57 -1000

    Tested: case where one of the Statisticians are empty

--- Data Structures/Statistician Extended/StatisticianDemo.java
+++ Data Structures/Statistician Extended/StatisticianDemo.java
@@ -27,5 +27,30 @@ public class StatisticianDemo {
         System.out.println("Is Minimum NaN? " + Double.isNaN(addTest.minimum()));
         System.out.println("Is Last Number NaN? " + Double.isNaN(addTest.lastNumber()));
         
-    }
+        
+        // Testing when one Statistician is empty 
+        addTest = Statistician.add(a, s2); 
+        // Expected Output: all statistics should be from Statistician a 
+        System.out.println("\nAdding a non-empty Statistician with an empty one");
+        System.out.println(
+            "Length: Expected = 3 | Actual = " + addTest.length() + "\n"
+            + "Sum: Expected = -3.0 | Actual = " + addTest.sum() + "\n"
+            + "Mean: Expected = -1.0 | Actual = " + addTest.mean() + "\n"
+            + "Maximum: Expected = 5.0 | Actual = " + addTest.maximum() + "\n"
+            + "Minimum: Expected = -10.0 | Actual = " + addTest.minimum() + "\n" 
+            + "Last Number: Expected = 5.0 | Actual = " + addTest.lastNumber()
+        );
+
+        addTest = Statistician.add(s1, c); 
+        System.out.println("\nAdding an empty Statistician with a non-empty one");
+        // Expected Output: All statistics should follow from Statistician c 
+        System.out.println(
+            "Length: Expected = 3 | Actual = " + addTest.length() + "\n"
+            + "Sum: Expected = -120.0 | Actual = " + addTest.sum() + "\n"
+            + "Mean: Expected = -40.0 | Actual = " + addTest.mean() + "\n"
+            + "Maximum: Expected = -10.0 | Actual = " + addTest.maximum() + "\n"
+            + "Minimum: Expected = -100.0 | Actual = " + addTest.minimum() + "\n" 
+            + "Last Number: Expected = -100.0 | Actual = " + addTest.lastNumber()
+        );
+    } 
 }
