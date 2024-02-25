7ddf6816e1c4d77df7b892f2bec7669679699f30
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-02-25 13:15:00 -1000

    Testing add method. 1st case: when both Statisticians are empty

--- Data Structures/Statistician Extended/StatisticianDemo.java
+++ Data Structures/Statistician Extended/StatisticianDemo.java
@@ -16,5 +16,20 @@ public class StatisticianDemo {
         c.nextNumber(-10.0); 
         c.nextNumber(-10.0); 
         c.nextNumber(-100.0); 
+
+        // Empty Statistician for testing. 
+        Statistician s1 = new Statistician(); 
+        Statistician s2 = new Statistician(); 
+
+
+        // Testing for adding empty Statisticians
+        Statistician addTest = Statistician.add(s1, s2); 
+        System.out.println("Adding two empty Statisticians s1 and s2");
+        System.out.println("Length: Expected = 0 | Actual = " + addTest.length());
+        System.out.println("Sum: Expected = 0 | Actual = " + addTest.sum());
+        System.out.println("Is Mean NaN? " + Double.isNaN(addTest.mean()));
+        System.out.println("Is Maximum NaN? " + Double.isNaN(addTest.maximum()));
+        System.out.println("Is Minimum NaN? " + Double.isNaN(addTest.minimum()));
+        
     }
 }
