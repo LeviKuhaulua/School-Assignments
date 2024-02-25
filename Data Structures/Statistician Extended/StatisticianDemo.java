e62920d1e1fa786f1b4aa2b640f5527598dc101f
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-02-25 12:40:56 -1000

    Created some Statistician instances for testing

--- Data Structures/Statistician Extended/StatisticianDemo.java
+++ Data Structures/Statistician Extended/StatisticianDemo.java
@@ -1,5 +1,20 @@
 public class StatisticianDemo {
     public static void main(String[] args) { 
-        System.out.println("Hello World");
+
+        // Creating Statistician objects for testing. 
+        Statistician a = new Statistician(); 
+        a.nextNumber(2.0);
+        a.nextNumber(-10.0); 
+        a.nextNumber(5.0); 
+
+        Statistician b = new Statistician(); 
+        b.nextNumber(1.0); 
+        b.nextNumber(2.0); 
+        b.nextNumber(3.0); 
+
+        Statistician c = new Statistician(); 
+        c.nextNumber(-10.0); 
+        c.nextNumber(-10.0); 
+        c.nextNumber(-100.0); 
     }
 }
