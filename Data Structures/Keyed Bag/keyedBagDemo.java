75fdd128fc9f99b78f3e9188fd353da3d7b9bb18
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-03-03 22:21:54 -1000

    Testing constructors for KeyedBag class

--- Data Structures/Keyed Bag/keyedBagDemo.java
+++ Data Structures/Keyed Bag/keyedBagDemo.java
@@ -1,5 +1,13 @@
 public class keyedBagDemo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        KeyedBag a = new KeyedBag(); 
+        KeyedBag b = new KeyedBag(5); 
+
+        System.out.println("Checking Constructors");
+        System.out.printf("KeyedBag a - Expected Capacity: 10 | Actual: %d %n", a.getCapacity());
+        System.out.printf("KeyedBag b - Expected Capacity: 5 | Actual: %d %n", b.getCapacity());
+        // b = new KeyedBag(-1); 
+
     }
 }
