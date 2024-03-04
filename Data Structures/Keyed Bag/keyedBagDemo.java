65dc9b1d7033c86467f5f573c8de7b69adc344a6
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-03-03 22:46:43 -1000

    Slight testing for Add and Size method of Keyed Bag

--- Data Structures/Keyed Bag/keyedBagDemo.java
+++ Data Structures/Keyed Bag/keyedBagDemo.java
@@ -1,13 +1,18 @@
 public class keyedBagDemo {
     public static void main(String[] args) {
         
-        KeyedBag a = new KeyedBag(); 
-        KeyedBag b = new KeyedBag(5); 
+        keyedBag a = new keyedBag(); 
+        keyedBag b = new keyedBag(5);  
 
-        System.out.println("Checking Constructors");
-        System.out.printf("KeyedBag a - Expected Capacity: 10 | Actual: %d %n", a.getCapacity());
-        System.out.printf("KeyedBag b - Expected Capacity: 5 | Actual: %d %n", b.getCapacity());
+        // System.out.println("Checking Constructors");
+        // System.out.printf("keyedBag a - Expected Capacity: 10 | Actual: %d %n", a.getCapacity());
+        // System.out.printf("keyedBag b - Expected Capacity: 5 | Actual: %d %n", b.getCapacity());
         // b = new KeyedBag(-1); 
 
+        System.out.println("\nChecking Add and Size method");
+        a.add(0, "Levi"); 
+        a.add(1, "Leviticus"); 
+        System.out.printf("Expected Size: 2 | Actual: %d %n", a.size());
+        a.add(-12, "Invalid"); 
     }
 }
