357897bd8cc80b511adf0eea1d164fb5fe9355d2
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-04-11 22:23:31 -1000

    Initial push for File Upload Assignment

---
+++ Data Communications/File Upload Assignment/Client.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader; 
+import java.io.InputStreamReader; 
+
+import java.net.Socket; 
+import java.net.UnknownHostException;
+import java.net.SocketException; 
+
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Howzit!");
+    }
+}
---
+++ Data Communications/File Upload Assignment/Server.java
@@ -0,0 +1,12 @@
+import java.net.ServerSocket; 
+import java.net.SocketException; 
+
+import java.io.BufferedReader; 
+import java.io.InputStreamReader; 
+import java.io.PrintWriter; 
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Howzit!");
+    }
+}
