7b94225db116cb66b94eac99b274bb14c5b8a807
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-04-13 19:47:00 -1000

    Code skeleton for Server and Client interaction

--- Data Communications/File Upload Assignment/Client.java
+++ Data Communications/File Upload Assignment/Client.java
@@ -1,5 +1,7 @@
-import java.io.BufferedReader; 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader; 
+import java.io.PrintWriter; 
 
 import java.net.Socket; 
 import java.net.UnknownHostException;
@@ -7,7 +9,31 @@ import java.net.SocketException;
 
 
 public class Client {
-    public static void main(String[] args) {
-        System.out.println("Howzit!");
+    public static void main(final String[] args) {
+        Socket client; 
+        String hostName = "127.0.0.1"; 
+        String line; 
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in)); 
+        
+        if (args.length > 0) {
+            hostName = args[0]; 
+        }
+
+        try {
+            client = new Socket(hostName, 12345); // Replace host name with IP Address you want to connect to. 
+            BufferedReader fromServer = new BufferedReader(new InputStreamReader(client.getInputStream())); 
+            PrintWriter toServer = new PrintWriter(client.getOutputStream(), true); 
+
+            System.out.println(fromServer.readLine());
+        } catch (UnknownHostException e) {
+            System.err.println("Unknown Host Name: " + hostName);
+            System.exit(-1); 
+        } catch (SocketException e) {
+            e.printStackTrace(); 
+            System.exit(-1); 
+        } catch (IOException e) {
+            e.printStackTrace(); 
+            System.exit(-1); 
+        }
     }
 }
--- Data Communications/File Upload Assignment/Server.java
+++ Data Communications/File Upload Assignment/Server.java
@@ -1,12 +1,32 @@
 import java.net.ServerSocket; 
 import java.net.SocketException; 
+import java.net.Socket; 
 
 import java.io.BufferedReader; 
 import java.io.InputStreamReader; 
-import java.io.PrintWriter; 
+import java.io.PrintWriter;
+import java.io.IOException;  
 
 public class Server {
     public static void main(String[] args) {
-        System.out.println("Howzit!");
+        
+        ServerSocket server; 
+        Socket client; 
+
+        try {
+            server = new ServerSocket(12345); 
+            client = server.accept(); 
+            
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream())); 
+            PrintWriter toClient = new PrintWriter(client.getOutputStream(), true); 
+
+            toClient.println("Aloha"); 
+        } catch (SocketException e) {
+            e.printStackTrace();
+            System.exit(-1); 
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1); 
+        }
     }
 }
