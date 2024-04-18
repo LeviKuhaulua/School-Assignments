1e9c8e63e008af914c3171d160186d7c2763a8b1
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-04-17 18:49:36 -1000

    Deleted .vscode folder since that's for launch configs for IDE

--- Data Communications/File Upload Assignment/Client.java
+++ Data Communications/File Upload Assignment/Client.java
@@ -1,55 +1,93 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader; 
-import java.io.PrintWriter; 
-
 import java.net.Socket; 
-import java.net.UnknownHostException;
-import java.net.SocketException; 
-
-import java.io.File;
+import java.net.UnknownHostException; 
+import java.io.IOException; 
+import java.io.DataInputStream; 
+import java.io.DataOutputStream; 
+import java.io.InputStreamReader; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.nio.file.Files; 
+import java.nio.file.Path; 
+import java.nio.file.Paths; 
+import static java.nio.file.StandardOpenOption.*; 
+import java.util.Arrays; 
 
 public class Client {
-    public static void main(final String[] args) throws IOException{
-        // Initializating variables. 
-        Socket client = null; 
-        String hostName = "127.0.0.1"; // Make default - localhost
-        String line; 
-        BufferedReader fromServer = null; 
-        PrintWriter toServer = null; 
 
-        // Use diff. hostname if one found in arguments of Command Line. 
+    private static void print(String[] arr) {
+        for (String i : arr) {
+            System.out.println(i);
+        }
+    }
+    public static void main(final String[] args) throws IOException{
+        
+        String hostname = "localhost";  // Default if no args specified when running file. 
+        BufferedReader fromServer = null; 
+        BufferedReader stdIn = null; 
+        PrintWriter toServer = null; 
+        String line = null; 
+        
+
+        // In case user specifies a hostname. 
         if (args.length > 0) {
-            hostName = args[0]; 
+            hostname = args[0]; 
         }
 
-        try {
-            client = new Socket(hostName, 12345); 
-            System.out.println("Connection to server established.");
+        
+        // Establish connection with server. 
+        try (Socket client = new Socket(hostname, 12345)) {
+            
+            fromServer = new BufferedReader(new InputStreamReader(client.getInputStream())); 
+            toServer = new PrintWriter(client.getOutputStream(), true); 
+
+            // Client Side: Send "Aloha" welcome message. 
+            toServer.println("Aloha"); 
+
+            while ((line = fromServer.readLine()) != null) {
+                if (line.trim().equalsIgnoreCase("Aloha Mai Kakou")) {
+                    System.out.println("Welcome message received. Starting file transfer.");
+                    break; 
+                } 
+            }
+
+
+            stdIn = new BufferedReader(new InputStreamReader(System.in)); 
+            Path dir = Paths.get(".\\"); 
+            String[] fileNames = Files.list(dir).map(f -> f.getFileName().toString()).toArray(String[]::new); 
+            
+            // Print out directory for user to pick a file. 
+            System.out.println("Pick a file to upload: "); 
+            print(fileNames); 
+            
+            // Continue to get user input if file does not match any of those found in the directory. 
+            while(Arrays.binarySearch(fileNames, (line = stdIn.readLine())) == -1) {
+                System.out.println("Pick a file to upload: \n");
+                print(fileNames); 
+            }
+
+            stdIn.close();
+            
+            // File chosen by user. 
+            Path fileToUpload = Paths.get(line); 
+            byte[] fileContentBytes; 
+
+            
+                       
+            
+            
         } catch (UnknownHostException e) {
-            System.err.println("Unknown Host: " + hostName);
+            System.err.println("Unknown Host: " + hostname);
             System.exit(-1); 
         } catch (IOException e) {
-            System.err.println("Socket error detected: " + e.getMessage());
+            System.err.println("Error connecting to server." + e.getMessage());
+            System.exit(1); 
+        } finally {
+            stdIn.close(); 
+            fromServer.close(); 
+            toServer.close(); 
         }
-
-        fromServer = new BufferedReader(new InputStreamReader(client.getInputStream())); 
-        toServer = new PrintWriter(client.getOutputStream(), true); 
-
-        toServer.println("Aloha"); 
-        
-        while ((line = fromServer.readLine()) != null) {
-            if (line.trim().equalsIgnoreCase("Aloha mai kakou")) {
-                System.out.println("Welcome message received. Beginning file transfer.");
-                break; 
-            }
-        }
-        
-
-        toServer.close(); 
-        fromServer.close(); 
-        client.close(); 
-
-        System.exit(0); 
     }
+
+    
+    
 }
