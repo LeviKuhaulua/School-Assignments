bff7a099cbefdf9a2c78bf469716f9edfc4d1984
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-09-10 22:40:16 -1000

    Initial push

---
+++ Program Structures/Age Calculator/age.c
@@ -0,0 +1,17 @@
+// Author: Levi Kuhaulua
+// Date Created: 09-10-2024
+/* Purpose: 
+*  The program will help us get accustomed to working with 
+*  user input and then converting them. The task is to get
+*  the user's birthdate and then calculate the age that they
+*  would be now. 
+*/
+
+#include <stdio.h>
+
+
+int main(void) {
+
+    puts("Hello World!"); 
+    return 0; 
+}
