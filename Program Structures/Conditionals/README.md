4b1c07d7e53062e3a5c864ebaa0827cc96310e22
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-09-16 20:08:15 -1000

    Initial Push

---
+++ Program Structures/Conditionals/README.md
@@ -0,0 +1,8 @@
+# Assignment
+
+Purpose: Understand how conditional statements work in C. 
+
+## Assignment Instructions
+
+- [ ] Prompt the user for a number from 0-9
+- [ ] Provide the equivalent word for that number in another language. 
---
+++ Program Structures/Conditionals/a06.c
@@ -0,0 +1,17 @@
+// Author: Levi Kuhaulua
+// Date Created: 09-16-2024
+/* Purpose: 
+*  The program will prompt a user to enter a number from 0-9. 
+*  After getting the number, the program will write the number
+*  in 'Olelo Hawai'i
+*/
+
+#include <stdio.h>
+#define PURPOSE "Program will display the Hawaiian word for a number of your choice."
+#define GETNUM "Enter a number from 0-9: "
+
+
+int main(void) {
+    printf(PURPOSE);
+    return 0;
+}
