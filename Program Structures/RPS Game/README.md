9eeadf6196975604cf92c37e5ec323845c1fdd70
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-09-28 20:35:29 -1000

    Initial push

---
+++ Program Structures/RPS Game/README.md
@@ -0,0 +1,11 @@
+# Assignment Overview
+
+To understand C Functions. This time, it is to help us with getting comfortable with creating our own C functions and using them in our program. 
+
+## Assignment Tasks
+
+- [ ] Create a Rock, Paper, Scissors Game
+- [ ] Game is continous until user inputs an EOF character
+- [ ] Before ending program, display game summary
+
+**Make sure to use 3 functions of your own**
---
+++ Program Structures/RPS Game/a09.c
@@ -0,0 +1,24 @@
+// Author: Levi Kuhaulua
+// Date Created: 09-28-2024
+/* Program Description:
+*  A rock, paper, scissors game against the computer. 
+*  This program allows you to continously play against
+*  the computer until an EOF character is submitted. 
+*  At the end of the program, it will also display a 
+*  game summary. 
+*/
+
+// Library and/or Macro Definitions
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+// Function Prototypes
+
+
+int main(void){
+    printf("Hello World");
+    return 0;
+}
