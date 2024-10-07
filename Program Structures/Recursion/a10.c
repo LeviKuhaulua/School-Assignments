87c997162de0cd9d8f31d952d445d4f4518b7ae5
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-06 21:19:47 -1000

    Implemented: recursive output, recursive adding

--- Program Structures/Recursion/a10.c
+++ Program Structures/Recursion/a10.c
@@ -12,25 +12,70 @@
 
 // Function Prototypes
 
+void displayRange(int start, int end); 
+int addSum(int start, int end);
+
 
 
 int main(void){
 
     // Grabbing input from user 
     printf("Enter two positive integers, separated by a space, the first smaller than the second: "); 
-    double num1 = getdouble();  
-    double num2 = getdouble(); 
+    int num1 = getdouble();  
+    int num2 = getdouble(); 
     
     // Checking for invalid input. 
     if (num1 < 0 || num2 < 0) {
         printf("Invalid Input: Both numbers must be positive"); 
-    } 
-
-    if (num1 > num2) {
+    } else if (num1 >= num2) {
         printf("Invalid Input: First number must be smaller than the second"); 
+    } else {
+
+        // Recursive Functions: 
+
+        // Display Range
+        printf("From %i to %i: ", num1, num2); 
+        displayRange(num1, num2); 
+
+        // Get Sum 
+        int sum = addSum(num1, num2); 
+        printf("Sum from %d to %d = %d", num1, num2, sum); 
+
     }
+
     
+
     return 0;
 }
 
+/*
+ * Output integers from start to end. 
+ * Start = first number inputted. 
+ * End = second number inputted. 
+ */
+void displayRange(int start, int end) {
+    // Base Case
+    if (start == end) {
+        printf("%i\n", end); 
+    } else {
+        // Recursive Call
+        printf("%i ", start); 
+        displayRange(start + 1, end); 
+    }
+}
 
+/*
+ * Recursive function to find the sum of integers from start to end. 
+ */
+int addSum(int start, int end) {
+    int result = 0; 
+    // Base Case
+    if (start == end) {
+        result = end; 
+    } else {
+        // Recursive Call
+        result = start + addSum(start + 1, end);
+    }
+
+    return result; 
+}
