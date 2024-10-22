3cecb5ee855f1389c4b8826fd595e3a8c0cb7eef
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-21 15:04:57 -1000

    Initial Push

---
+++ Program Structures/Arrays/README.md
@@ -0,0 +1,8 @@
+# Assignment Overview
+Learning about how arrays work in C. 
+
+# Assignment Instructions
+
+- [ ] Prompt user to input letters from the alphabet (both upper and lower case)
+- [ ] Use an array to count the amount of letters that show up. 
+- [ ] Output the amount of times a letter shows up to terminal.   
---
+++ Program Structures/Arrays/a11.c
@@ -0,0 +1,17 @@
+// Author: Levi Kuhaulua
+// Date Created: 10-21-2024
+/* Program Description:
+*  The program will prompt the user to enter letters
+*  in the alphabet (both lower and upper case) and 
+*  output the amount of times the letter shows up
+*/
+
+// Library and/or Macro Definitions
+#include <stdio.h>
+
+// Function Prototypes
+
+int main(void){
+    printf("Hello World");
+    return 0;
+}
