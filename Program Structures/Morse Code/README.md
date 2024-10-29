5c470239a3cd400c8c3254ffced026ef67b9be3b
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-29 12:27:53 -1000

    Initial Push

---
+++ Program Structures/Morse Code/README.md
@@ -0,0 +1,10 @@
+# Assignment Overview
+
+Understand how to work with characters and strings in C. 
+
+# Assignment Instructions
+
+- [ ] Enter morse code as command line arguments
+- [ ] Translate the morse code
+- [ ] Display translated message to terminal
+
---
+++ Program Structures/Morse Code/a15.c
@@ -0,0 +1,22 @@
+// Author: Levi Kuhaulua
+// Date Created: 10-29-2024
+/* Program Description:
+*  Program will take in Morse Code from the command line
+*  arguments, then translate and display them to standard 
+*  output. 
+*/
+
+// Library and/or Macro Definitions
+#include <stdio.h>
+
+// Libraries associated with String and Characters. Update libraries when needed. 
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h> 
+
+// Function Prototypes
+
+int main(void){
+    printf("Hello World");
+    return 0;
+}
