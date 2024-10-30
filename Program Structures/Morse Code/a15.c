790bed7bc85d87f0beceb9e3ae2670790505722a
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-10-29 14:44:06 -1000

    Translated morse code arguments, and removed libraries not in use

--- Program Structures/Morse Code/a15.c
+++ Program Structures/Morse Code/a15.c
@@ -11,10 +11,10 @@
 
 // Libraries associated with String and Characters. Update libraries when needed. 
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h> 
+#include <stdbool.h>
 
 // Function Prototypes
+bool checkLength(int, int); 
 
 
 // Defining Morse Code Dictionary
@@ -65,5 +65,51 @@ int main(int argc, char *argv[]){
         return 0; 
     }
 
+    // Initializing an array based on the count of the arguments. 
+    char *morseWords[argc - 1]; 
+
+    // Used to check if morse word command argument is a substring in the morse dictionary.
+    char *substr = NULL; 
+
+    // Used to compare and check length of strings. 
+    int lengthMorse = 0; 
+    int lengthArg = 0; 
+
+    for (int i = 1; i < argc; i++) {
+
+        for (int j = 0; j < 36; j++) {
+            substr = strstr(morse[j], argv[i]); 
+
+            /*
+            *  Length of string in morse array follows format: 
+            *  CHARACTER + SPACE + MORSE + SPACE
+            *  CHARACTER = morse translation
+            */
+            lengthMorse = strlen(morse[j]); 
+
+            // 3 represents the CHARACTER, SPACE, SPACE in format specified above ^. 
+            lengthArg = strlen(argv[i]) + 3; 
+            
+            // Output morse code if it matches these conditions. 
+            if (substr != NULL && checkLength(lengthMorse, lengthArg)) {
+                printf("%c", morse[j][0]); 
+            } 
+        }
+
+    }
+
+
     return 0;
+}
+
+/* checkLength Function:
+*  Params: 
+*   - length, integer. Represents the length of a word. 
+*   - argLength, integer. Represents the length of the word argument inputted in terminal. 
+*  
+*  Returns True if length of word is equal to the length of the word argument, otherwise False. 
+*
+*/
+bool checkLength(int length, int argLength) {
+    return length == argLength; 
 }
