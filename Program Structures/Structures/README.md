d8df946de3c32b36b9e876daadf1af6cc1aac64f
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-02 18:33:27 -1000

    Initial Push

---
+++ Program Structures/Structures/README.md
@@ -0,0 +1,16 @@
+# Assignment Overview
+
+To understand and learn how to use Structures in C. 
+
+# Assignment Instructions
+
+- [ ] Create a Card Struct
+- [ ] Shuffle the cards and display the following: 
+    - Suit
+    - Rank
+    - Color
+
+**Additional Information**: 
+
+- Card structure is already created. Modifying original code. 
+
---
+++ Program Structures/Structures/a17.c
@@ -0,0 +1,16 @@
+// Author: Levi Kuhaulua
+// Date Created: 11-02-2024
+/* Program Description:
+*  Program shuffles and shows the deck of cards to the user. 
+*  The suit, rank, and color is displayed to the user. 
+*/
+
+// Library and/or Macro Definitions
+#include <stdio.h>
+
+// Function Prototypes
+
+int main(int argc, char *argv[]){
+    printf("Hello World");
+    return 0;
+}
