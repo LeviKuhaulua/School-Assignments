693ca3ef7c10762151e64b9e0347ac3700213887
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-12 21:38:00 -1000

    Impl: Updating record

--- Program Structures/Files/a18.c
+++ Program Structures/Files/a18.c
@@ -27,6 +27,10 @@ int main(void){
     int fieldEdit = -1; 
 
     char *userInput[3] = {"\0"}; 
+    int sizeName = 0; 
+    int age = 0; 
+    double gpa = 0.0; 
+    char dummyString[MAX_STRING] = {'\0'}; 
     // To hold student records pulled from file. 
     Student student = {-1, "", "", 0, 0.0}; 
 
@@ -104,14 +108,66 @@ int main(void){
         printf("- (3) age\n"); 
         printf("- (4) gpa\n"); 
 
-        fieldEdit = (int) getdouble() - 1; 
+        fieldEdit = (int) getdouble(); 
 
         // Check if what user entered was invalid. 
-        while (fieldEdit < 0 || fieldEdit > 3) {
+        while (fieldEdit < 1 || fieldEdit > 4) {
             printf("Invalid Input: %i\n", fieldEdit + 1); 
             printf("Enter (1) for first name, (2) for last name, (3) for age, or (4) for gpa: "); 
-            fieldEdit = (int) getdouble() - 1; 
+            fieldEdit = (int) getdouble(); 
         }
+
+        // Edit specific field information
+        switch (fieldEdit) {
+            // First two cases (1) and (2) deal with editing name fields. 
+            case 1: 
+                sizeName = getstring(student.first, MAX_STRING); 
+                while (sizeName > MAX_STRING - 2) {
+                    printf("Invalid input. First name cannot be longer than %d characters\n", MAX_STRING - 2);
+                    getline2(dummyString, MAX_STRING); 
+                    sizeName = getstring(student.first, MAX_STRING); 
+                }
+                break; 
+            case 2: 
+                sizeName = getstring(student.last, MAX_STRING); 
+                while (sizeName > MAX_STRING - 2) {
+                    printf("Invalid input. Last name cannot be longer than %d characters\n", MAX_STRING - 2); 
+                    sizeName = getstring(student.last, MAX_STRING); 
+                }
+                break; 
+            // Last two cases deal with editing number fields. 
+            case 3: 
+                printf("Age: ");
+                age = (int) getdouble(); 
+                while (age <= 0) {
+                    printf("Invalid input: %i\n", age); 
+                    printf("Age: "); 
+                    age = (int) getdouble(); 
+                } 
+                student.age = age; 
+                break; 
+
+            case 4: 
+                printf("GPA: "); 
+                gpa = getdouble(); 
+                while (gpa <= 0) {
+                    printf("Invalid input: %.2f\n", gpa); 
+                    gpa = getdouble(); 
+                }
+                student.gpa = gpa; 
+                break; 
+
+            // This shouldn't occur... 
+            default: 
+                printf("Editing a non-existent field"); 
+                return -1; 
+        }
+
+        // Writing updates to a file. 
+        fseek(filePointer, rowEdit * sizeof(Student), SEEK_SET); 
+        fwrite(&student, sizeof(Student), 1, filePointer); 
+        rewind(filePointer); 
+        printRecords(filePointer, student); 
 
 
     } while (!doneEditing);
