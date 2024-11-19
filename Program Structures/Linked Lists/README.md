be2df2f69f8812e4da183067a61195fa72e88aa8
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-19 13:09:24 -1000

    Initial Push

---
+++ Program Structures/Linked Lists/README.md
@@ -0,0 +1,10 @@
+# Assignment Overview
+
+Understand what is a Linked List, how they work, and how to implement. 
+
+# Assignment Instructions
+
+- [ ] insert 25 to 75 random integers from 0-100 into a linked list 
+- [ ] output the sum of the integers in the linked list
+- [ ] output the number of integers in the linked list
+- [ ] output the floating point average
---
+++ Program Structures/Linked Lists/a19.c
@@ -0,0 +1,17 @@
+// Author: Levi Kuhaulua
+// Date Created: 11-19-2024
+/* Program Description:
+*  The program inputs a 25-75 integers inside a linked list (ranging from 0-100). 
+*  It will then output the following data: sum, count, and floating point average. 
+*/
+
+// Library and/or Macro Definitions
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(void){
+	printf("Hello World");
+    return 0;
+}
