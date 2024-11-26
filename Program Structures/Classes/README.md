c178de94c579b7b613ef23044df2df7cca245eed
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-26 10:56:35 -1000

    Initial Push

---
+++ Program Structures/Classes/README.md
@@ -0,0 +1,5 @@
+# Assignment Overview
+
+Moving on from C to C++. Learning and understanding how C++ classes work. 
+
+# Assignment Tasks
---
+++ Program Structures/Classes/a20.cpp
@@ -0,0 +1,17 @@
+// Author: Levi Kuhaulua
+// Date Created: 11-26-2024
+/* Program Description:
+*  Creates a class labeled Complex that allows users to 
+*  perform arithmetic operations with complex numbers. 
+*/
+
+
+#include <iostream> 
+using namespace std; 
+int main(void) {
+    cout << "Enter a number: ";
+    double number = 0.0; 
+    cin >> number;
+    cout << "Number entered: " << number;
+    return 0;
+}
