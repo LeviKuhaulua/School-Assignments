367eeb3ca7109f8cebabd89f279989d9330dd301
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2024-11-27 19:49:54 -1000

    Impl: Circle class

--- Program Structures/Classes4/a23.cpp
+++ Program Structures/Classes4/a23.cpp
@@ -8,10 +8,74 @@
 
 
 #include <iostream>
+
+// Error checking for negative values
+#include <stdexcept>
 using namespace std;
+ 
+class Circle {
+    
+    public: 
+        
+        static constexpr double PI = 3.1415926;
+        
+        // Initializes a Circle with a radius of 0
+        Circle() {
+            radius = 0; 
+        } 
+        
+        // Initializes a Circle with radius 'r'
+        Circle(double r) {
+            // Error checking to see if radius that is passed is negative
+            if (isNegativeRadius(r)) {
+                throw invalid_argument("Radius cannot be negative!");
+            }
+            radius = r;
+        }
+        
+        // Copy constructor that initializes a Circle based on another Circle's radius
+        Circle(Circle &c) {
+            radius = c.radius; 
+        }
+        
+        // Sets a Circle's radius to r 
+        void setRadius(double r) {
+            // Error checking to see if radius that is passed is negative
+            if (isNegativeRadius(r)) {
+                throw invalid_argument("Radius cannot be negative!");
+            }
+            radius = r; 
+        }
+        
+        // Returns the radius
+        double getRadius() {
+            return radius;
+        }
+        
+        // Calculates the area of a circle. Formula: PI * r * r
+        double calculateArea() {
+            return PI * radius * radius;
+        }
+        
+        // Prints out a Circle's information (in this case, it's radius)
+        void print() const {
+            cout << "Circle with Radius " << radius << "\n";
+        }
+
+    protected: 
+        double radius; 
+    
+    private: 
+        // Returns true if the radius is negative, otherwise, false
+        bool isNegativeRadius(double r) {
+            return r < 0;
+        }
+};
 
 
 int main(void){
-    cout << "Hello World!";
+    Circle a(3);
+    cout << a.calculateArea();
+    
     return 0;
 }
