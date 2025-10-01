a0975bb855d17140b26e816fecfba1d56e84bfeb
Levi Kuhaulua <levi.kuhaulua@outlook.com>
2025-10-01 08:47:45 -1000

    Completed Part 2 (output to file)

--- Operating Systems/Homework 4/command_executor/solution.c
+++ Operating Systems/Homework 4/command_executor/solution.c
@@ -17,6 +17,7 @@ void execute_plain(char *cmd, char *const argv[]) {
   // checks if we can fork
   if (pid == -1) {
     perror("fork()");
+    fprintf(stdout, "** COMMAND FAILED **\n");
     exit(1);
   }
 
@@ -49,20 +50,48 @@ void execute_plain(char *cmd, char *const argv[]) {
  *  that redirects the command's output to a file.
  */
 void execute_output_to_file(char *cmd, char *const argv[], char *filename) {
-  // fprintf(stdout,"Error: %s() is not implemented!\n\n", __FUNCTION__);
 
-  // WRITE CODE HERE
+  FILE *file = fopen(filename, "a+");  
+ 
+  // check if can open file
+  if (!file) {
+    perror("fopen()");
+    fprintf(stdout, "** COMMAND FAILED **");
+    exit(1);
+  }
+ 
   pid_t pid = fork();
 
+  // check if can fork
   if (pid == -1) {
     perror("fork()");
+    fprintf(stdout, "** COMMAND FAILED **");
     exit(1);
   }
 
   if (!pid) {
     // CHILD
+    // close stdout
+    close(1);
+    // create stream to file for commands
+    dup(file->_fileno);
+    exit(execv(cmd, argv));
+  } else {
+    // PARENT
+    int returnStatus;
+    waitpid(pid, &returnStatus, 0);
+
+    returnStatus = WEXITSTATUS(returnStatus);
+    
+    // stdout should work b/c we closed stdout stream in child process not parent
+    if (!returnStatus) {
+      fprintf(stdout, "** COMMAND SUCCESSFUL **\n");
+    } else {
+      fprintf(stdout, "** COMMAND FAILED **\n");
+    }
   }
 }
+
 
 /* 
  * A function that executes a command using fork and execv, but
